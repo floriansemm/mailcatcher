4a96a5e9c6ddff6019cd42577ec1fa3f48cd312c
floriansemm <floriansemm@gmail.com>
2014-04-28 11:54:09 +0200

    basic dummy dashboard with bootstrap 3

--- web/index.php
+++ web/index.php
@@ -4,10 +4,13 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/', function() {
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
 
+$app->get('/', function() use ($app) {
 
-
+    return $app['twig']->render('index.html.twig');
 
 });
 
