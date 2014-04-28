4bb3965e52a65275d11505cde97ab0829593b493
floriansemm <floriansemm@gmail.com>
2014-04-28 16:26:42 +0200

    show mails in overview

--- web/index.php
+++ web/index.php
@@ -8,9 +8,18 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
 
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_sqlite',
+        'path'     => __DIR__.'/../app/mailcatcher.sqlite',
+    ),
+));
+
 $app->get('/', function() use ($app) {
 
-    return $app['twig']->render('index.html.twig');
+    $sentMails = $app['db']->fetchAll("SELECT * FROM sent_mails");
+
+    return $app['twig']->render('index.html.twig', array('mails' => $sentMails));
 
 });
 
