f2bc2b7f5787496db47432236a686c0e820b7830
floriansemm <floriansemm@gmail.com>
2014-04-29 09:59:37 +0200

    - add startscript for cli-server - add codeblock to server assets
    should close #2

--- web/index.php
+++ web/index.php
@@ -2,6 +2,11 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
 $app = new Silex\Application();
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
